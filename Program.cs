51c096002011d7d7cb354fec384ba79832003684
Enrique Rodriguez <stirgoy@hotmail.com>
2022-12-09 17:11:34 +0100

    Revert "Revert "User and app settings: import and export""
    This reverts commit 4f1a24a1c02e690840b06791344912e96ef9edb7.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 
 namespace BUM
 {
+    [Serializable]
     internal static class Program
     {
         /// <summary>
---
+++ core/crypto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace BUM
+{
+    internal class crypto
+    {
+    }
+    public partial class frmBUM
+    {
+        string encrypt(string toCrypt)
+        {
+            string result = string.Empty;
+            byte[] encryted = System.Text.Encoding.Unicode.GetBytes(toCrypt);
+            result = Convert.ToBase64String(encryted);
+            return result;
+        }
+
+        string decrypt(string toDecrypt)
+        {
+            string result = string.Empty;
+            byte[] decryted;
+            try
+            {
+                decryted = Convert.FromBase64String(toDecrypt);
+
+            }
+            catch (FormatException)
+            {
+                decryted = System.Text.Encoding.Unicode.GetBytes(toDecrypt); //<<<== bypass for old versions
+            }
+                result = System.Text.Encoding.Unicode.GetString(decryted);
+                return result;
+        }
+    }
+}
